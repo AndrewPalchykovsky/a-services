e2b790eb1a6e828fa6a8f01ad6b945d6afff5f06
Andrew Palchykovsky <dangerok2@gmail.com>
2020-07-14 16:57:01 +0300

    cross component communication

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -6,12 +6,15 @@ import { AccountService } from '../account.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  // providers: [LoggingService]
 })
 export class NewAccountComponent {
 
 
-  constructor(private loggingService: LoggingService, private accountService: AccountService) { }
+  constructor(private loggingService: LoggingService, private accountService: AccountService) {
+    this.accountService.statusUpdated.subscribe(
+      (status) => alert('New status ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountStatus, accountStatus);
